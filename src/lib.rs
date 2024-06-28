1936536facf69293fd3383472f337d8be3b8cd1d
Nicolas SIMON <nicolas.simon6790@gmail.com>
2024-06-28 18:20:12 +0200

    Change SmolDb::set_database_file parameter type to PathBuf instead of &Path
     #3

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,17 @@
-#[doc = include_str!("../README.md")]
-mod db_model;
+use std::fs;
+use std::path::PathBuf;
 
-use std::{fs, path::Path};
+use rustlog::write_log;
 
 pub use db_model::{DbModel, DbTable};
-use rustlog::write_log;
+
+#[doc = include_str!("../README.md")]
+mod db_model;
 
 #[derive(PartialEq, Debug)]
 pub struct SmolDb {
     model: DbModel,
-    db_file: Option<&'static Path>,
+    db_file: Option<PathBuf>,
 }
 
 impl SmolDb {
@@ -21,37 +23,37 @@ impl SmolDb {
         }
     }
 
-    /// Load databse from file
-    pub fn load(db_file: &'static Path) -> Result<SmolDb, String> {
-        match fs::read_to_string(db_file) {
+    /// Load database from file
+    pub fn load(db_file: PathBuf) -> Result<SmolDb, String> {
+        match fs::read_to_string(&db_file) {
             Ok(json) => {
                 match serde_json::from_str::<DbModel>(&json) {
                     Ok(model) => {
                         Ok(SmolDb {
                             model,
-                            db_file: Some(db_file)
+                            db_file: Some(db_file),
                         })
-                    },
+                    }
                     Err(e) => {
-                        let msg = format!("{}",e);
+                        let msg = format!("{}", e);
                         write_log(
                             rustlog::LogSeverity::Error,
                             &msg,
                             &env!("CARGO_PKG_NAME").to_string(),
                         );
                         Err(msg)
-                    },
+                    }
                 }
-            },
+            }
             Err(e) => {
-                let msg = format!("{}",e);
+                let msg = format!("{}", e);
                 write_log(
                     rustlog::LogSeverity::Error,
                     &msg,
                     &env!("CARGO_PKG_NAME").to_string(),
                 );
                 Err(msg)
-            },
+            }
         }
     }
 
@@ -61,7 +63,7 @@ impl SmolDb {
     }
 
     /// Sets path to database file
-    pub fn set_database_file(&mut self, file: &'static Path) {
+    pub fn set_database_file(&mut self, file: PathBuf) {
         self.db_file = Some(file)
     }
 
@@ -69,15 +71,15 @@ impl SmolDb {
     pub fn save(&self) -> Result<(), String> {
         if self.db_file.is_some() {
             match serde_json::to_string(&self.model) {
-                Ok(json) => match fs::write(self.db_file.unwrap(), json) {
+                Ok(json) => match fs::write(self.db_file.as_ref().unwrap(), json) {
                     Ok(_) => {
                         write_log(
                             rustlog::LogSeverity::Info,
-                            &format!("Database saved to file {}", self.db_file.unwrap().to_str().unwrap()),
+                            &format!("Database saved to file {}", self.db_file.as_ref().unwrap().display()),
                             &env!("CARGO_PKG_NAME").to_string(),
                         );
                         Ok(())
-                    },
+                    }
                     Err(e) => {
                         let msg = format!("{}", e);
                         write_log(
@@ -86,10 +88,10 @@ impl SmolDb {
                             &env!("CARGO_PKG_NAME").to_string(),
                         );
                         Err(msg)
-                    },
+                    }
                 },
                 Err(e) => {
-                    let msg = format!("{}",e);
+                    let msg = format!("{}", e);
                     write_log(
                         rustlog::LogSeverity::Error,
                         &msg,
@@ -98,8 +100,7 @@ impl SmolDb {
                     Err(msg)
                 }
             }
-        }
-        else {
+        } else {
             let msg = format!("Cannot save database, no database file configured");
             write_log(
                 rustlog::LogSeverity::Warning,
@@ -113,7 +114,8 @@ impl SmolDb {
 
 #[cfg(test)]
 mod tests {
-    use std::{fs::remove_file, path::Path};
+    use std::fs::remove_file;
+    use std::path::PathBuf;
 
     use rusttests::{check_result, check_struct, CheckType};
 
@@ -123,7 +125,7 @@ mod tests {
     fn save_no_file() -> Result<(), String> {
         let db = SmolDb::init("db_name".to_string());
 
-        check_result((1,1),db.save(), false)?;
+        check_result((1, 1), db.save(), false)?;
         Ok(())
     }
 
@@ -131,15 +133,14 @@ mod tests {
     fn save_and_load() -> Result<(), String> {
         let mut db = SmolDb::init("db_name".to_string());
 
-        db.set_database_file(Path::new("test.json"));
+        db.set_database_file(PathBuf::from("file.json"));
 
-        check_result((1,1), db.save(), true)?;
+        check_result((1, 1), db.save(), true)?;
 
-        let new_db = SmolDb::load(Path::new("test.json"))?;
+        let new_db = SmolDb::load(PathBuf::from("file.json"))?;
 
-        remove_file("test.json").unwrap_or(());
+        remove_file("file.json").unwrap_or(());
 
-        check_struct((2,1), &new_db, &db, CheckType::Equal)
-
+        check_struct((2, 1), &new_db, &db, CheckType::Equal)
     }
 }
